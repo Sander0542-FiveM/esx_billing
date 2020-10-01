5b66ece12237f5f261ebe7e2ee71d84f691be2af
Sander Jochems <sanderjochems@hotmail.nl>
2020-10-01 18:23:49 +0200

    Add Dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,12 @@
+Locales['nl'] = {
+  ['invoices'] = 'rekeningen',
+  ['invoices_item'] = '€%s',
+  ['received_invoice'] = 'u heeft zojuist een rekening ~r~ontvangen~s~',
+  ['paid_invoice'] = 'je hebt een rekening van ~r~€%s~s~ ~g~betaald~s~',
+  ['no_invoices'] = 'je hoeft op dit moment geen rekeningen te betalen',
+  ['received_payment'] = 'je hebt een rekening van ~r~€%s~s~ ~g~ontvangen~s~',
+  ['player_not_online'] = 'de speler is niet ingelogd',
+  ['no_money'] = 'u heeft niet genoeg geld om deze rekening te betalen',
+  ['target_no_money'] = 'de speler ~r~heeft niet~s~ genoeg geld om de rekening te betalen!',
+  ['keymap_showbills'] = 'open rekeningen menu',
+}
